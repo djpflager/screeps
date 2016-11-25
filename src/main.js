f0be9c78e42219b947b3fcc8ba66e19556062faf
Jamie Pflager <djpflager@gmail.com>
2016-11-24 19:19:44 -0800

    move creeps to a switch, add an empty spawners file

--- src/main.js
+++ src/main.js
@@ -1,18 +1,31 @@
 //@flow
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
+var roleHarvester = require('./role.harvester.js');
+var roleUpgrader = require('./role.upgrader.js');
+var roleBuilder = require('./role.builder.js');
+var spawners = require('./spawners.js');
+
+function runCreepRoles() {
+  for (var name : string in Game.creeps) {
+    let creep = Game.creeps[name];
+    switch (creep.memory.role) {
+      case 'harvester':
+        roleHarvester.run(creep);
+        break;
+      case 'upgrader':
+        roleUpgrader.run(creep);
+        break;
+      case 'builder':
+        roleBuilder.run(creep);
+        break;
+      default:
+        creep.say('Unassigned');
+    }
+  }
+  // TODO: add defense role
+}
 
 module.exports.loop = function () {
-    for (var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-    }
+    runCreepRoles();
+    spawners.run();
+    // TODO: add construction sites logic
 }
---
+++ src/spawners.js
@@ -0,0 +1,5 @@
+function run() {
+
+}
+
+modules.exports = { run() }
